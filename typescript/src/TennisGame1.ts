9df27eb422b9ecced105e65f86c1af5e973427dc
Gabriele Toselli <gabriele.toselli@golee.it>
2021-06-08 09:31:26 +0200

    Raggruppata logica casistiche

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -15,50 +15,31 @@ export class TennisGame1 implements TennisGame {
   }
 
   public getScore(): string {
-    let score: string = "";
-    let tempScore: number = 0;
     // Caso 1: parità tra due giocatori
     if (this.arePlayerTied()) {
-      score = this.player_1.getScoreName();
+      return this.player_1.getScoreNameTie();
     }
+
     // Caso 2: uno dei due giocatori ha superato i 4 punti
     else if (this.player_1.getScore() >= 4 || this.player_2.getScore() >= 4) {
-      const minusResult: number =
-        this.player_1.getScore() - this.player_2.getScore();
-      if (minusResult === 1) score = "Advantage player1";
-      else if (minusResult === -1) score = "Advantage player2";
-      else if (minusResult >= 2) score = "Win for player1";
-      else score = "Win for player2";
+      return this.getAdvantageScoreName();
     }
+
     // Caso 3: nessuno dei due giocatori è arrivato ancora ai 4 punti
-    else {
-      for (let i = 1; i < 3; i++) {
-        if (i === 1) tempScore = this.player_1.getScore();
-        else {
-          score += "-";
-          tempScore = this.player_2.getScore();
-        }
-        switch (tempScore) {
-          case 0:
-            score += "Love";
-            break;
-          case 1:
-            score += "Fifteen";
-            break;
-          case 2:
-            score += "Thirty";
-            break;
-          case 3:
-            score += "Forty";
-            break;
-        }
-      }
-    }
-    return score;
+    return `${this.player_1.getScoreName()}-${this.player_2.getScoreName()}`;
   }
 
   private arePlayerTied = (): boolean =>
     this.player_1.getScore() === this.player_2.getScore();
+
+  private getAdvantageScoreName = (): string => {
+    const score_diff: number =
+      this.player_1.getScore() - this.player_2.getScore();
+    if (score_diff === 1) return "Advantage player1";
+    else if (score_diff === -1) return "Advantage player2";
+    else if (score_diff >= 2) return "Win for player1";
+    else return "Win for player2";
+  };
 }
 
 class Player {
@@ -68,17 +49,32 @@ class Player {
     new Player(data.name, data.score);
 
   public getName = (): string => this.name;
+
   public getScore = (): number => this.score;
+
+  public getScoreNameTie = (): string => {
+    switch (this.score) {
+      case 0:
+        return ETieScoreName.love_all;
+      case 1:
+        return ETieScoreName.fifteen_all;
+      case 2:
+        return ETieScoreName.thirty_all;
+      default:
+        return ETieScoreName.deuce;
+    }
+  };
+
   public getScoreName = (): string => {
     switch (this.score) {
       case 0:
-        return EScoreName.love_all;
+        return EScoreName.love;
       case 1:
-        return EScoreName.fifteen_all;
+        return EScoreName.fifteen;
       case 2:
-        return EScoreName.thirty_all;
-      default:
-        return EScoreName.deuce;
+        return EScoreName.thirty;
+      case 3:
+        return EScoreName.forty;
     }
   };
 
@@ -88,6 +84,14 @@ class Player {
 }
 
 enum EScoreName {
+  love = "Love",
+  fifteen = "Fifteen",
+  thirty = "Thirty",
+  deuce = "Deuce",
+  forty = "Forty",
+}
+
+enum ETieScoreName {
   love_all = "Love-All",
   fifteen_all = "Fifteen-All",
   thirty_all = "Thirty-All",
